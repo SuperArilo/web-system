a29711a7dc8b98cc559aca65bf182371b014c91f
caoguirong <cao184771@aliyun.com>
2021-12-21 13:31:47 +0800

    添加描述

--- src/main/java/com/example/backgroundsystem/vo/DynamicVO.java
+++ src/main/java/com/example/backgroundsystem/vo/DynamicVO.java
@@ -5,16 +5,34 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * 动态展示区数据视图对象
+ */
 @Data
 public class DynamicVO extends Dynamic {
 
+    /**
+     * 用户名称
+     */
     private String username;
 
+    /**
+     * 头像地址
+     */
     private String headAddress;
 
+    /**
+     * 用户类别
+     */
     private Integer clazz;
 
+    /**
+     * 动态图片集合
+     */
     private List<String> imageAddresses;
 
+    /**
+     * 动态点赞人名称集合
+     */
     private List<String> usersWhoLikedIt;
 }
