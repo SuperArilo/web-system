246db40ce07c00504fa55a98d151ca722e7dbc80
caoguirong <cao184771@aliyun.com>
2022-02-10 17:43:10 +0800

    bug

--- src/main/java/online/superarilo/myblog/config/MybatisConfig.java
+++ src/main/java/online/superarilo/myblog/config/MybatisConfig.java
@@ -1,29 +1,29 @@
-//package online.superarilo.myblog.config;
-//
-//
-//import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
-//import online.superarilo.myblog.utils.SnowFlakeUtil;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.UUID;
-//
-//@Configuration
-//public class MybatisConfig {
-//
-//    @Bean
-//    public IdentifierGenerator identifierGenerator() {
-//        return new IdentifierGenerator() {
-//            @Override
-//            public String nextUUID(Object entity) {
-//                return IdentifierGenerator.super.nextUUID(entity);
-//            }
-//
-//            @Override
-//            public Number nextId(Object entity) {
-//                SnowFlakeUtil snowFlake = new SnowFlakeUtil(1L,1L);
-//                return snowFlake.nextId();
-//            }
-//        };
-//    }
-//}
+package online.superarilo.myblog.config;
+
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import online.superarilo.myblog.utils.SnowFlakeUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public IdentifierGenerator identifierGenerator() {
+        return new IdentifierGenerator() {
+            @Override
+            public String nextUUID(Object entity) {
+                return IdentifierGenerator.super.nextUUID(entity);
+            }
+
+            @Override
+            public Number nextId(Object entity) {
+                SnowFlakeUtil snowFlake = new SnowFlakeUtil(1,1,1);
+                return snowFlake.nextId();
+            }
+        };
+    }
+}
