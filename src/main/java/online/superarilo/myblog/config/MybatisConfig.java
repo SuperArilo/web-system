fc05aa3911a1957e1ffdd48899e5b5b8cf1ed4eb
caoguirong <cao184771@aliyun.com>
2022-02-11 11:24:13 +0800

    id

--- src/main/java/online/superarilo/myblog/config/MybatisConfig.java
+++ src/main/java/online/superarilo/myblog/config/MybatisConfig.java
@@ -2,18 +2,25 @@ package online.superarilo.myblog.config;
 
 
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
-import online.superarilo.myblog.utils.SnowFlakeUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.UUID;
 
+
 @Configuration
 public class MybatisConfig {
 
+    private static int inc = 0;
+
     @Bean
     public IdentifierGenerator identifierGenerator() {
+
         return new IdentifierGenerator() {
+
+            private static final long LIMIT = 10000000000L;
+            private static long last = 0;
+
             @Override
             public String nextUUID(Object entity) {
                 return IdentifierGenerator.super.nextUUID(entity);
@@ -21,8 +28,15 @@ public class MybatisConfig {
 
             @Override
             public Number nextId(Object entity) {
-                SnowFlakeUtil snowFlake = new SnowFlakeUtil(1,1,1);
-                return snowFlake.nextId();
+                // 10 digits.
+                long id = System.currentTimeMillis() % LIMIT;
+                if ( id <= last ) {
+                    id = (last + 1) % LIMIT;
+                }
+                return last = id;
+
+//                SnowFlakeUtil snowFlake = new SnowFlakeUtil(1,1,1);
+//                return snowFlake.nextId();
             }
         };
     }
