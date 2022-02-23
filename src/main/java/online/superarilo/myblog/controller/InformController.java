93485934780816d8c8d7da5b96252773ea4dbabc
caoguirong <cao184771@aliyun.com>
2022-02-23 11:10:42 +0800

    添加通知bug修复

--- src/main/java/online/superarilo/myblog/controller/InformController.java
+++ src/main/java/online/superarilo/myblog/controller/InformController.java
@@ -3,13 +3,12 @@ package online.superarilo.myblog.controller;
 
 import online.superarilo.myblog.service.IInformService;
 import online.superarilo.myblog.utils.JsonResult;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -19,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author caoguirong
  * @since 2022-02-23
  */
-@Controller
+@RestController
 @RequestMapping("/inform")
 public class InformController {
 
